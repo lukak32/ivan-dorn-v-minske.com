660273f9d23230a802e07e19f39f3e51239a8d00
lukak32 <42774534+lukak32@users.noreply.github.com>
2020-02-19 19:17:23 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function show() {
+    document.getElementById("amphitheatre").classList.add("buy__amphitheatre--open");
+}
+
+function hide() {
+    document.getElementById("amphitheatre").classList.remove("buy__amphitheatre--open");
+}
